f5c958ba9cb082c3eeafeee35f32965edb70004d
shamaahlogbi <candlelogbi@outlook.com>
2025-10-09 21:29:19 +0300

    Add constant

---
+++ lib/network/constant_api.dart
@@ -0,0 +1,11 @@
+class ConstantApi{
+  final baseURL="https://api-week.onrender.com";
+ // final authLoginEndPoint="/auth/login";
+  final folderEndPoint="/folders";
+  final createFolderEndPoint="/folders/create";
+  final updateFolderEndPoint="/folders/update";
+  final deleteFolderEndPoint="/folders/delete";
+  Map<String,String> header = {"Accept": "application/json"};
+
+
+}
